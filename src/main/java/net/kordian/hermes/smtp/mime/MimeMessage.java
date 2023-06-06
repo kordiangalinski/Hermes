78b51aa844badc6e7c49846cee084ffd5c88db66
Kodian Galinski <kordian.dev@gmail.com>
2023-06-06 12:49:02 +0200

    first commit

--- src/main/java/net/kordian/hermes/smtp/mime/MimeMessage.java
+++
@@ -1,7 +0,0 @@
-package net.kordian.hermes.smtp.mime;
-
-public class MimeMessage {
-    private MimeMessage message;
-//    private MimeBodyPart body;
-//    private List<MimeBodyPart> attachments = new ArrayList<>();
-}
